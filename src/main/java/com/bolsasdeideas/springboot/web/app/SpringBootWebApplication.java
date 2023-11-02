16c58fabfaf87d13ac8911b9d1c82ee7a8129ec3
Francisco Javier Jimenez <>
2023-11-02 15:37:35 +0100

    added html template, thymealf and get mapping

--- src/main/java/com/bolsasdeideas/springboot/web/app/SpringBootWebApplication.java
+++ src/main/java/com/bolsasdeideas/springboot/web/app/SpringBootWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBbotWebApplication {
+public class SpringBootWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBbotWebApplication.class, args);
+		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
 
 }
--- src/main/java/com/bolsasdeideas/springboot/web/app/controller/IndexController.java
+++ src/main/java/com/bolsasdeideas/springboot/web/app/controller/IndexController.java
@@ -1,5 +1,17 @@
 package com.bolsasdeideas.springboot.web.app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+
+    // @GetMapping("/index")
+    @GetMapping(value = {"/index", "/", "/home"})
+    public String index(Model model){
+        model.addAttribute("title", "Hallo Spring Framework");
+        return "index";
+    }
 
 }
